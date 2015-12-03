da250c21427af4a206d745abd9b076b52c15f361
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-12-03 14:31:05 -0200

    Project Euler 003

---
+++ Project Euler/ProjectEuler003.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#define INF 800000000000
+#define false 0
+#define true 1
+
+int is_prime(int n){
+
+    if( n <= 2 ) 
+    	return true;
+
+    if( n % 2 == 0 ) 
+    	return false;
+    
+    for( int i = 3 ; i <= n/i ; i = i+ 2 )
+        if( n % i == 0 ) 
+        	return false;
+    
+    return true;
+}
+
+int next_prime(int primo){
+
+	int numero = primo + 1;
+
+	while( !is_prime(numero) ){
+
+		numero++;
+	}
+
+	return numero;
+
+}
+
+long long maior_fator(long long number){
+
+	long long numero = number;
+
+	int next = next_prime(1);
+
+	while(1){
+
+		if ( numero % next == 0 ){
+
+			numero = numero/next;
+		}else if ( numero == 1 ){
+
+			return next;
+		}else{
+
+			next = next_prime(next);
+		}
+	}
+
+}
+
+int main(){
+
+	/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+	*/
+
+	printf("%lld\n", maior_fator(600851475143) );
+
+	return 0;
+}
