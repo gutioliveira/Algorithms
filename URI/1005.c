e33ca078b7a42b90a0d5ca9dfdb5c3e3a6909dd0
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-12-06 09:32:15 -0200

    URI-1005 Média 1

---
+++ URI/1005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    double a,b,c;
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+
+    c = (a*3.5 + b*7.5)/11;
+
+    printf("MEDIA = %.5f\n", c);
+
+    return 0;
+}
+1
