402a42e092f27672a5b1200f72e0c8ad3f1ee606
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2016-07-20 12:00:09 -0300

    Changing algorithm to store the sequence

--- Algorithms/Dynamic Programming/longest_increasing_subsequence.cpp
+++ Algorithms/Dynamic Programming/longest_increasing_subsequence.cpp
@@ -6,12 +6,9 @@ int main(){
 	
 	int array[] = {3,4,-1,0,6,2,3};
 
+	int actual_solution[] = {-1,-1,-1,-1,-1,-1,-1}; // stores the actual subsequence.
+
 	int values[] = {1,1,1,1,1,1,1};
-
-	map < int, vector<int> > mapa; // stores every number on the LIS not sorted.
-
-	for ( int i = 0; i < 7; i++ )
-		mapa[i].push_back(array[i]); 
 
 	// LIS
 	for ( int i = 1; i < 7; i++ )
@@ -20,7 +17,7 @@ int main(){
 			if ( array[i] > array[j] )
 				if ( values[j]+1 > values[i] ){
 
-					mapa[i].push_back(array[j]); //
+					actual_solution[i] = j;
 
 					values[i] = values[j]+1;
 				}
@@ -28,8 +25,22 @@ int main(){
 	//
 	printf("\n");
 
+	map < int, vector<int> > mapa;
+
 	for ( int i = 0; i < 7; i++ ){
 		printf("LIS size = %d\n", values[i]); // prints size of LIS.
+
+		int te = i;
+
+		while(1){ // puts on a map the solution.
+
+			if ( te == -1 )
+				break;
+
+			mapa[i].push_back(array[te]);
+
+			te = actual_solution[te];
+		}
 	
 		printf("numbers on the subsequence.");
 		// for ( int i = 0; i < 7; i++ ){
